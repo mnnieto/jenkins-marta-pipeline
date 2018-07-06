2ba5cfb7cde7f5b2ad6c4a842b3c8519db58c817
mnnieto <mnnieto@gmail.com>
2018-07-06 15:26:49 +0200

    testing

--- vars/martaPipeline.groovy
+++ vars/martaPipeline.groovy
@@ -14,21 +14,33 @@ import hudson.plugins.git.util.*
 import hudson.plugins.git.opt.*
 import hudson.plugins.git.extensions.*
 import hudson.plugins.git.extensions.impl.*
-
+/**
+ * martaPipeline.groovy
+ *
+ * Marta Pipeline
+ * @param body	Closure of execution
+ *
+ * Inside the body of the pipeline, it's possible to provide the following pairs key - value as pipeline parameters
+ *
+ */
 def call(body) {
 
     /** evaluate the body block, and collect configuration into the object **/
     def config = [:]
-    def pipelineTest
+    def pipelineTest = null;
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
     /** **/
-    stage('\u2776 Initialize') {
+    stage('Initialize') {
         echo "\u2776 Initialize"
-        pipelineTest = configPipeline.initialize(config)
+        
         echo "Pipeline " + pipelineTest.jdkVersion
     }
-
+    stage('Checking-QA') {
+        echo "\u2776 Initialize"
+        
+        echo "Pipeline " + pipelineTest.jdkVersion
+    }
 }
