ce7dcb13af9cb29147247b77af44d2cbbd85e8e4
Marta <32172177+mnnieto@users.noreply.github.com>
2019-11-06 11:14:38 +0100

    Update martaPipeline.groovy

--- vars/martaPipeline.groovy
+++ vars/martaPipeline.groovy
@@ -1,19 +1,6 @@
 #!/usr/bin/env groovy
 
-import groovy.json.JsonSlurper
-import org.jfrog.hudson.*
-import hudson.plugins.git.*
-import hudson.plugins.git.util.*
-import hudson.plugins.git.opt.*
-import hudson.plugins.git.extensions.*
-import hudson.plugins.git.extensions.impl.*
-import groovy.json.JsonSlurper
-import org.jfrog.hudson.*
-import hudson.plugins.git.*
-import hudson.plugins.git.util.*
-import hudson.plugins.git.opt.*
-import hudson.plugins.git.extensions.*
-import hudson.plugins.git.extensions.impl.*
+
 /**
  * martaPipeline.groovy
  *
@@ -23,13 +10,9 @@ import hudson.plugins.git.extensions.impl.*
  * Inside the body of the pipeline, it's possible to provide the following pairs key - value as pipeline parameters
  *
  */
-def call(body) {
+def call() {
 
-    /** evaluate the body block, and collect configuration into the object **/
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+
 
     /** **/
     stage('Initialize') {
